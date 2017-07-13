577d1ff999144d9f01f14d3fc143332ed49ec78c
renaultJB <j.baptiste.renault@gmail.com>
2017-07-13 17:16:36 +0200

    Comment TriOpenMesh.m

--- CoreSubFunctions/TriOpenMesh.m
+++ CoreSubFunctions/TriOpenMesh.m
@@ -1,6 +1,8 @@
 function [ TRout ] = TriOpenMesh( TRsup , TRin , nbElmts )
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+%TriOpenMesh : Perform a "open" morphological operation on a triangulation object 
+% First a erosion by nbElmts of TRin
+% Second an dilatation by nbElmts of the newly created triangulation object TR
+
 
 [ TR ] = TriErodeMesh( TRin, nbElmts );
 [ TRout ] = TriDilateMesh( TRsup, TR, nbElmts );

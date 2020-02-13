7aeb798467f8256f01f2562a6144b170831861f6
renaultJB <j.baptiste.renault@gmail.com>
2020-02-13 11:54:48 +0100

    Modify ReadMesh to GMSH 4.X

--- Output/Analysis/rmdl_LoadShare.R
+++ Output/Analysis/rmdl_LoadShare.R
@@ -14,23 +14,88 @@ Rmdl_11 <- read_csv("OutputLoadCentrality_RMDL11.txt")
 Rmdl_11$subjectCode <- as.factor(Rmdl_11$subjectCode)
 Rmdl_11$LegSide <- as.factor(Rmdl_11$LegSide)
 Rmdl_11$ImplantSize <- as.factor(Rmdl_11$ImplantSize)
-Rmdl_11$alpha <- as.factor(Rmdl_11$alpha)
+Rmdl_11$alpha.F <- as.factor(Rmdl_11$alpha)
+Rmdl_11$Epoch.F <- as.factor(Rmdl_11$Epoch)
 Rmdl_11$Lshare100 <- 100*Rmdl_11$Lshare
+Rmdl_11$Lshare.Top <- 100*Rmdl_11$Lsup/Rmdl_11$Ltot
+Rmdl_11$Min.Mean <- 2*Rmdl_11$Min.Mean
 Rmdl_11$Rmdl.TBCMT <- rep("Yes",length(Rmdl_11$Lshare))
 Rmdl_11$Rmdl.BONE <- rep("Yes",length(Rmdl_11$Lshare))
 Rmdl_11$Rmdl <- rep("Both",length(Rmdl_11$Lshare))
+revalue(Rmdl_11$Alignement, c("Kine"="Cinématique", "Mech"="Mécanique")) -> Rmdl_11$Alignement
+
+Rmdl_11_init <- Rmdl_11
 
 
 
 
 
+# Rmdl_11 <- subset(Rmdl_11, subjectCode == 'GUI_R' )
 
-Rmdl_11 <- subset(Rmdl_11, subjectCode == 'DES_R' )
+########################################################################################
+### Plot Lshare vs centrality
+########################################################################################
+# & LoadStep == "GC_50"
+Rmdl_11 <- subset(Rmdl_11, Epoch == 0 | Epoch == 12 | Epoch == 36 | Epoch == 72 & LoadStep != "CU"  )
+Rmdl_11 <- aggregate(Lshare100 ~ Epoch.F + alpha + Min.Mean + Alignement + CV , Rmdl_11, FUN = 'mean')
+ggplot(Rmdl_11, aes(Min.Mean, Lshare100 , color = Epoch.F, fill = Epoch.F )) + 
+  geom_point(size=3) + theme_classic() +
+  stat_smooth(method = "lm", formula = y ~ x + I(x^2), size = 1) +
+  xlab('Centrality.MM') + ylab('Fprop (%)') +
+  theme(axis.text=element_text(size=16), axis.title = element_text(size=16)) +
+  theme(legend.text=element_text(size=16)) +
+  labs(fill = "Mois") + labs(color = "Mois")
+
+ggplot(Rmdl_11, aes(Min.Mean, Lshare100 , color = Epoch.F, fill = Epoch.F )) + 
+  geom_point(size=3) + theme_classic() +
+  stat_smooth(method = "lm") +
+  xlab('Centrality.MM') + ylab('Fprop (%)') +
+  theme(axis.text=element_text(size=16), axis.title = element_text(size=16)) +
+  theme(legend.text=element_text(size=16)) +
+  labs(fill = "Mois") + labs(color = "Mois")
+
+ggplot(Rmdl_11, aes(alpha, Lshare100 , color = Epoch.F, fill = Epoch.F )) + 
+  geom_point(size=3) + theme_classic() +
+  stat_smooth(method = "lm", formula = y ~ x + I(x^2), size = 1) +
+  xlab('HKA (°)') + ylab('Fprop (%)') +
+  theme(axis.text=element_text(size=16), axis.title = element_text(size=16)) +
+  theme(legend.text=element_text(size=16)) +
+  labs(fill = "Mois") + labs(color = "Mois")
+
+Rmdl_11 <- Rmdl_11_init
+Rmdl_11 <- subset(Rmdl_11, Epoch == 0 | Epoch == 12 | Epoch == 36 | Epoch == 72 & LoadStep != "CU"  )
+Rmdl_11 <- aggregate(Lshare.Top ~ Epoch.F + Min.Mean + Alignement + CV , Rmdl_11, FUN = 'mean')
+ggplot(Rmdl_11, aes(Min.Mean, Lshare.Top , color = Epoch.F, fill = Epoch.F )) + 
+  geom_point(size=3) + theme_classic() +
+  stat_smooth(method = "lm", formula = y ~ x + I(x^2), size = 1) +
+  xlab('Centrality.MM') + ylab('Fprop Top (%)') +
+  theme(axis.text=element_text(size=16), axis.title = element_text(size=16)) +
+  theme(legend.text=element_text(size=16)) +
+  labs(fill = "Mois") + labs(color = "Mois")
 
 
+########################################################################################
+### Plot Lshare vs centrality
+########################################################################################
+Rmdl_11 <- Rmdl_11_init
+Rmdl_11 <- subset(Rmdl_11, (alpha = alpha != 3.5= 0 | alpha == -3.48)  & subjectCode == 'TRO_L'  )
 
 
+bar_all <- ggplot(Rmdl_11, aes(x = Epoch.F, y = Lshare100 , fill = Alignement)) + 
+  geom_bar(stat="identity", position="dodge")+
+  theme_classic() + xlab('Mois') + ylab('Fprop (%)') + 
+  theme(axis.text=element_text(size=20), axis.title = element_text(size=20))
+bar_all + facet_grid(. ~ LoadStep)
 
+Rmdl_11 <- Rmdl_11_init
+Rmdl_11 <- subset(Rmdl_11, (alpha != -1.75 | alpha != 1.75 | alpha != 3.5))
+bar_all <- ggplot(Rmdl_11, aes(x = Epoch.F, y = Lshare100 , fill = Alignement)) + 
+  geom_bar(stat="identity", position="dodge")+
+  theme_classic() + xlab('Mois') + ylab('Fprop (%)') + 
+  theme(axis.text=element_text(size=12), axis.title = element_text(size=16))
+bar_all + facet_grid(subjectCode ~ LoadStep) +
+  theme(strip.text.x = element_text(size = 16)) +
+  theme(strip.text.y = element_text(size = 16))
 
 ########################################################################################
 ### Read data from remodelling of both bone only
@@ -46,6 +111,8 @@ Rmdl_10$Lshare100 <- 100*Rmdl_10$Lshare
 Rmdl_10$Rmdl.TBCMT <- rep("No",length(Rmdl_10$Lshare))
 Rmdl_10$Rmdl.BONE <- rep("Yes",length(Rmdl_10$Lshare))
 Rmdl_10$Rmdl <- rep("Both",length(Rmdl_10$Lshare))
+revalue(Rmdl_10$Alignement, c("Kine"="Cinématique", "Mech"="Mécanique")) -> Rmdl_10$Alignement
+
 
 
 ########################################################################################
@@ -90,7 +157,7 @@ DF2$Epoch <- as.factor(DF2$Epoch)
 bar_all <- ggplot(DF2, aes(x = Epoch, y = Lshare100 , fill = Alignement)) + 
   geom_bar(stat="identity", position="dodge")+
   theme_classic() + xlab('Mois') + ylab('Fprop (%)') + 
-  theme(axis.text=element_text(size=14), axis.title = element_text(size=20))
+  theme(axis.text=element_text(size=20), axis.title = element_text(size=20))
 bar_all + facet_grid(. ~ LoadStep)
 
 DF_mean <- aggregate(Lshare100 ~ Mois + Alignement + Align.Rmdl.TBCMT + Rmdl.TBCMT, DF, FUN = 'mean')
@@ -113,7 +180,8 @@ pt <- ggplot(DF_mean, aes(Mois, Lshare100 , color = Rmdl.TBCMT, shape = Aligneme
   geom_smooth(method = 'loess', se=FALSE) +
   xlab('Mois') +
   ylab('Fprop (%)') + 
-  theme(axis.text=element_text(size=14), axis.title = element_text(size=20))
+  theme(axis.text=element_text(size=16), axis.title = element_text(size=16)) +
+  theme(legend.text=element_text(size=16), legend.position= c(0.8, 0.2))
 pt
 
 point_all <- ggplot(DF_mean, aes(x = Mois, y = Lshare100 , color = Align.Rmdl.TBCMT)) + 
